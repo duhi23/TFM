b93aadafc9493a8cf2fbd7a24288b7e86cd2616b
duhi23 <diego.21.phs@hotmail.com>
2018-05-22 01:53:34 +0200

    Código Final

--- Datos/tidy_data.R
+++ Datos/tidy_data.R
@@ -90,7 +90,7 @@ ctasas <- function(dxt,ext,ini,fin){
       return(res)
 }
 # data.frame(Asc=seq(1,65), Des=seq(65,1), Year=colnames(Dxt_h))
-
+# 16=1965 - 55=2004
 mxt_m <- ctasas(Dxt_m,Ext_m,16,55)
 mxt_h <- ctasas(Dxt_h,Ext_h,16,55)
 mxt_t <- ctasas(Dxt_t,Ext_t,16,55)
@@ -124,8 +124,6 @@ dataC_h <- StMoMoData(demogdata(data=mxt_h, pop=Ext_h[,c(16:55)], ages=c(0:105),
 dataC_t <- StMoMoData(demogdata(data=mxt_t, pop=Ext_t[,c(16:55)], ages=c(0:105), years=c(1965:2004), 
                      type="mortality", label="Spain", name="total"), type="central")
 
-#rm(list=c("mxt_m", "mxt_h", "mxt_t", "qxt_m", "qxt_h", "qxt_t", "Pxt_m", "Pxt_h", "Pxt_t",
-#          "Ext_m", "Ext_h", "Ext_t", "Dxt_m", "Dxt_h", "Dxt_t", "suavizar"))
 
 
 ################################################
@@ -139,41 +137,37 @@ LCfit_t <- fit(LC, data = dataC_t)
 rm(list = c("LC"))
 
 # Gráfico parámetro ax
-plot(LCfit_m$ax, type='o', col=2, pch=20, ylim=c(-10,0), xlab='', ylab='')
+plot(LCfit_m$ax, type='b', col=2, pch=18, ylim=c(-10,0), xlab='', ylab='')
 par(new=TRUE)
-plot(LCfit_h$ax, type='o', col=3, pch=20, ylim=c(-10,0), xlab='', ylab='')
-#par(new=TRUE)
-#plot(LCfit_t$ax, type='o', col=4, pch=18, ylim=c(-10,0), xlab='Edad', ylab='ax')
-#op <- par(cex = 2.8)
-legend(60, -5, legend=c("Mujeres", "Hombres"), col=c("red", "green"), cex=0.9, lwd=3, bty = 'n')
+plot(LCfit_h$ax, type='b', col=3, pch=20, ylim=c(-10,0), xlab='', ylab='')
+legend(60, -5, legend=c("Hombres","Mujeres"), col=c("green","red"),  bty = 'n', cex=0.9, 
+       pt.cex=2, inset=0.2, pch=c(20,18))
 
 
 # Gráfico parametro bx
-plot(LCfit_m$bx, type='o', col=2, pch=20, ylim=c(-0.045,0.05), xlab='', ylab='')
+plot(LCfit_m$bx, type='b', col=2, pch=18, ylim=c(-0.045,0.05), xlab='', ylab='')
 par(new=TRUE)
-plot(LCfit_h$bx, type='o', col=3, pch=20, ylim=c(-0.045,0.05), xlab='', ylab='')
-#par(new=TRUE)
-#plot(LCfit_t$bx, type='o', col=4, pch=18, ylim=c(-0.035,0.05), xlab='Edad', ylab='bx')
+plot(LCfit_h$bx, type='b', col=3, pch=20, ylim=c(-0.045,0.05), xlab='', ylab='')
 abline(h=0,lty=3)
-legend(45, 0.005, legend=c("Mujeres", "Hombres"), col=c("red", "green"), cex=0.9, lwd=3, bty = 'n')
+legend(45, 0.001, legend=c("Hombres","Mujeres"), col=c("green","red"), bty = 'n', cex=0.9, 
+       pt.cex=2, inset=0.2, pch=c(20,18))
 
 
 # Gráfico parametro kt
-plot(seq(1965,2004),as.numeric(LCfit_m$kt), type='o', col=2, pch=20, ylim=c(-55,55), xlab='', ylab='')
+plot(seq(1965,2004),as.numeric(LCfit_m$kt), type='b', col=2, pch=18, ylim=c(-50,50), xlab='', ylab='')
 par(new=TRUE)
-plot(seq(1965,2004),as.numeric(LCfit_h$kt), type='o', col=3, pch=20, ylim=c(-55,55), xlab='', ylab='')
-#par(new=TRUE)
-#plot(seq(1965,2004),as.numeric(LCfit_t$kt), type='o', col=4, pch=18, ylim=c(-55,55), xlab='Periodo', ylab='kt')
+plot(seq(1965,2004),as.numeric(LCfit_h$kt), type='b', col=3, pch=20, ylim=c(-50,50), xlab='', ylab='')
 abline(h=0,lty=3)
-legend(1985, 60, legend=c("Hombres", "Mujeres"), col=c("red", "green"), cex=0.9, lwd=3, bty = 'n')
+legend(1990, 50, legend=c("Hombres","Mujeres"), col=c("green","red"),  bty = 'n', cex=0.9, 
+       pt.cex=2, inset=0.2, pch=c(20,18))
 
 
 # Proyección parámetro kt
 est_m <- forecast(LCfit_m, h=50, level=c(90,95))
 est_h <- forecast(LCfit_h, h=50, level=c(90,95))
 est_t <- forecast(LCfit_t, h=50, level=c(90,95))
-plot(est_m, only.kt=TRUE, col='lightgreen')
-plot(est_h, only.kt=TRUE, col='lightpink')
+plot(est_h, only.kt=TRUE, col='lightgreen')
+plot(est_m, only.kt=TRUE, col='lightpink')
 plot(est_t, only.kt=TRUE, col='lightblue')
 
 # Comprobación modelos arima
@@ -191,16 +185,23 @@ for_qxt <- function(est){
       return(m)
 }
 
+# Acota las probabilidades a 1
+
+q1 <- function(mat){
+      res <- apply(mat, 2, function(x){ifelse(x>1,1,x)})
+      return(res)
+}
+
 
 # Gráfico tridimensional - Log tasas de mortalidad proyectadas
-z <- log(for_qxt(est_m))
+z <- log(q1(for_qxt(est_m)))
 edad <- seq(0, 105, by=1)
 tiempo <- seq(2005, 2054, by=1)
 color <- colorRampPalette(c("green", "yellow", "orange", "red"))(50)
 zfacet <- z[-1, -1] + z[-1, -length(tiempo)] + z[-length(edad), -1] + z[-length(edad), -length(tiempo)]
 facetcol <- cut(zfacet, 50)
 persp(edad, tiempo, z, theta=-30, phi=30, expand=0.75, xlab='Edad', col=color[facetcol],
-      ylab='Periodo', zlab='', ticktype="detailed", zlim=c(min(z),0.95))
+      ylab='Periodo', zlab='', ticktype="detailed", zlim=c(min(z),0.1))
 rm(list=c("z", "edad", "tiempo", "color", "zfacet", "facetcol"))
 
 
@@ -216,16 +217,17 @@ fun_logit <- function(qxt){
 
 # Gráfico logit qxt
 q <- qxt_m
-plot(fun_logit(q)[,1], col=2, type='p', ylim=c(-1.2,9.5), pch=18, ylab='')
+plot(fun_logit(q)[,1], col=2, type='p', ylim=c(-1.2,9.7), pch=18, ylab='')
 par(new=TRUE)
-plot(fun_logit(q)[,11], col=3, type='p', ylim=c(-1.2,9.5), pch=20, ylab='')
+plot(fun_logit(q)[,11], col=3, type='p', ylim=c(-1.2,9.7), pch=20, ylab='')
 par(new=TRUE)
-plot(fun_logit(q)[,21], col=4, type='p', ylim=c(-1.2,9.5), pch=18, ylab='')
+plot(fun_logit(q)[,21], col=4, type='p', ylim=c(-1.2,9.7), pch=18, ylab='')
 par(new=TRUE)
-plot(fun_logit(q)[,31], col=5, type='p', ylim=c(-1.2,9.5), pch=20, ylab='')
+plot(fun_logit(q)[,31], col=5, type='p', ylim=c(-1.2,9.7), pch=20, ylab='')
 par(new=TRUE)
-plot(fun_logit(q)[,40], col=6, type='p', ylim=c(-1.2,9.5), pch=18, ylab='logit(qxt)')
-legend(6, 6, legend=c("1965", "1975", "1985", "1995", "2004"), col=c(2, 3,4,5,6), cex=0.9, lwd=3, bty = 'n')
+plot(fun_logit(q)[,40], col=6, type='p', ylim=c(-1.2,9.7), pch=18, ylab='logit(qxt)')
+legend(6, 7, legend=c("1965", "1975", "1985", "1995", "2004"), col=c(2,3,4,5,6),  bty = 'n', cex=0.9, 
+       pt.cex=2, inset=0.2, pch=c(18,20,18,20,18))
 rm(list=c("q"))
 
 # Modelo
@@ -236,27 +238,25 @@ CBDfit_t <- fit(CBD, data = data0_t, ages.fit = 0:105, years=1965:2004)
 rm(list=c("CBD"))
 
 # Gráfico parámetro kt(1)
-plot(seq(1965,2004),CBDfit_m$kt[1,], type='o', col=2, pch=20, ylim=c(-7,-4), xlab='', ylab='')
+plot(seq(1965,2004),CBDfit_m$kt[1,], type='o', col=2, pch=18, ylim=c(-7,-4), xlab='', ylab='')
 par(new=TRUE)
 plot(seq(1965,2004),CBDfit_h$kt[1,], type='o', col=3, pch=20, ylim=c(-7,-4), xlab='', ylab='')
-#par(new=TRUE)
-#plot(seq(1965,2004),CBDfit_t$kt[1,], type='o', col=4, pch=18, ylim=c(-3,-1.5), xlab='Edad', ylab='kt(1)')
-legend(1980, -5.8, legend=c("Hombres", "Mujeres"), col=c("red", "green"), cex=0.9, lwd=3, bty = 'n')
+legend(1980, -5.8, legend=c("Hombres", "Mujeres"), col=c("green", "red"),  bty = 'n', cex=0.9, 
+       pt.cex=2, inset=0.2, pch=c(20,18))
 
 # Gráfico parámetro kt(2)
 plot(seq(1965,2004),CBDfit_m$kt[2,], type='o', col=2, pch=20, ylim=c(0.04,0.13), xlab='', ylab='')
 par(new=TRUE)
 plot(seq(1965,2004),CBDfit_h$kt[2,], type='o', col=3, pch=20, ylim=c(0.04,0.13), xlab='', ylab='')
-#par(new=TRUE)
-#plot(seq(1965,2004),CBDfit_t$kt[2,], type='o', col=4, pch=18, ylim=c(0.07,0.15), xlab='Edad', ylab='kt(2)')
-legend(1980, 0.07, legend=c("Hombres", "Mujeres"), col=c("red", "green"), cex=0.9, lwd=3, bty = 'n')
+legend(1980, 0.077, legend=c("Hombres", "Mujeres"), col=c("green", "red"),  bty = 'n', cex=0.9, 
+       pt.cex=2, inset=0.2, pch=c(20,18))
 
 # Proyección parámetro kt
 pro_m <- forecast(CBDfit_m, h=50, level=c(90,95))
 pro_h <- forecast(CBDfit_h, h=50, level=c(90,95))
 pro_t <- forecast(CBDfit_t, h=50, level=c(90,95))
-plot(pro_m, only.kt=TRUE, col='lightgreen')
-plot(pro_h, only.kt=TRUE, col='lightpink')
+plot(pro_m, only.kt=TRUE, col='lightpink')
+plot(pro_h, only.kt=TRUE, col='lightgreen')
 plot(pro_t, only.kt=TRUE, col='lightblue')
 
 
@@ -286,9 +286,10 @@ lg_qxt <- function(pro){
       return(q)
 }
 
+lg_qxt(pro_h) - pro_h$rates
 
 # Gráfico tridimensional - Log tasas de mortalidad
-z <- log(pro_h$rates)
+z <- log(pro_m$rates)
 edad <- seq(0, 105, by=1)
 tiempo <- seq(2005, 2054, by=1)
 color <- colorRampPalette(c("green", "yellow", "orange", "red"))(50)
@@ -299,6 +300,11 @@ persp(edad, tiempo, z, theta=-30, phi=30, expand=0.75, xlab='Edad', col=color[fa
 rm(list=c("z", "edad", "tiempo", "color", "zfacet", "facetcol"))
 
 
+plot(log(pro_h$rates)[,1], type='b', col=2, ylim=c(-12,0))
+par(new=TRUE)
+plot(log(pro_h$rates)[,10], type='b', col=3, ylim=c(-12,0))
+par(new=TRUE)
+plot(log(pro_h$rates)[,20], type='b', col=4, ylim=c(-12,0))
 
 
 ########################################
@@ -317,26 +323,30 @@ LCT_h <- lca(dataL_h, max.age = 105)
 LCT_t <- lca(dataL_t, max.age = 105)
 
 # Gráfico parámetro ax - Hombres & Mujeres
-plot(LCT_m$ax, type='o', col=2, pch=20, ylim=c(-10,0), xlab='', ylab='')
+plot(LCT_m$ax, type='b', col=2, pch=18, ylim=c(-10,0), xlab='', ylab='')
 par(new=TRUE)
-plot(LCT_h$ax, type='o', col=3, pch=20, ylim=c(-10,0), xlab='', ylab='')
-legend(60, -5, legend=c("Mujeres", "Hombres"), col=c("red", "green"), cex=0.9, lwd=3, bty = 'n')
+plot(LCT_h$ax, type='b', col=3, pch=20, ylim=c(-10,0), xlab='', ylab='')
+legend(60, -5, legend=c("Hombres","Mujeres"), col=c("green","red"),  bty = 'n', cex=0.9, 
+       pt.cex=2, inset=0.2, pch=c(20,18))
 
 # Gráfico parámetro Bx - Total
-plot(LCT_t$bx, type='o', col=4, pch=20, ylim=c(-0.026,0.036), xlab='', ylab='')
+plot(LCT_t$bx, type='o', col=4, pch=15, ylim=c(-0.026,0.036), xlab='', ylab='')
 abline(h=0,lty=3)
-legend(50, -0.003, legend=c("Total"), col=c("blue"), cex=0.9, lwd=3, bty = 'n')
+legend(50, -0.003, legend=c("Total"), col=c("blue"),  bty = 'n', cex=0.9, 
+       pt.cex=2, inset=0.2, pch=c(15))
 
 # Gráfico parámetro Kt - Total
-plot(LCT_t$kt, type='o', col=4, pch=20, ylim=c(-38,32), xlab='', ylab='')
+plot(LCT_t$kt, type='o', col=4, pch=15, ylim=c(-38,32), xlab='', ylab='')
 abline(h=0,lty=3)
-legend(1980, -15, legend=c("Total"), col=c("blue"), cex=0.9, lwd=3, bty = 'n')
+legend(1980, -15, legend=c("Total"), col=c("blue"),   bty = 'n', cex=0.9, 
+       pt.cex=2, inset=0.2, pch=c(15))
 
 
-res_m <- log(qxt_m)-LCT_m$ax-fm(LCT_t$bx,2)%*%fm(LCT_t$kt,1)
-pres_m <- log(qxt_m)-LCT_m$ax
-res_h <- log(qxt_h)-LCT_h$ax-fm(LCT_t$bx,2)%*%fm(LCT_t$kt,1)
-pres_h <- log(qxt_h)-LCT_h$ax
+res_m <- log(mxt_m)-LCT_m$ax-fm(LCT_t$bx,2)%*%fm(LCT_t$kt,1)
+pres_m <- log(mxt_m)-LCT_m$ax
+res_h <- log(mxt_h)-LCT_h$ax-fm(LCT_t$bx,2)%*%fm(LCT_t$kt,1)
+pres_h <- log(mxt_h)-LCT_h$ax
+
 
 # Ratio ajuste factor común
 Rc_m <- 1-(sum(res_m^2))/(sum(pres_m^2))
@@ -347,33 +357,33 @@ rm(list = c("Rc_m", "Rc_h"))
 
 # SVD residuos
 svd_res <- function(residuos){
-      res <- residuos - apply(residuos, 1, mean)
+      res <- residuos #- apply(residuos, 1, mean)
       const <- sum(svd(res)$u[,1])
       b <- svd(res)$u[,1]/const
       k <- const*max(svd(res)$d)*svd(res)$v[,1]
       return(list(bx=b, kt=k))
 }
- 
 
  
 # Gráfico parámetro bx - Hombres & Mujeres
-plot(svd_res(res_m)$bx, type='o', col=2, pch=20, ylim=c(-0.015,0.055), xlab='', ylab='')
+plot(svd_res(res_m)$bx, type='o', col=2, pch=18, ylim=c(-0.015,0.06), xlab='', ylab='')
 par(new=TRUE)
-plot(svd_res(res_h)$bx, type='o', col=3, pch=20, ylim=c(-0.015,0.055), xlab='', ylab='')
+plot(svd_res(res_h)$bx, type='o', col=3, pch=20, ylim=c(-0.015,0.06), xlab='', ylab='')
 abline(h=0,lty=3)
-legend(50, 0.055, legend=c("Mujeres", "Hombres"), col=c("red", "green"), cex=0.9, lwd=3, bty = 'n')
-
+legend(50, 0.055, legend=c("Hombres","Mujeres"), col=c("green","red"),  bty = 'n', cex=0.9, 
+       pt.cex=2, inset=0.2, pch=c(20,18))
 
 # Gráfico parámetro kt - Hombres & Mujeres
-plot(svd_res(res_m)$kt, type='o', col=2, pch=20, ylim=c(-15,15), xlab='', ylab='')
+plot(svd_res(res_m)$kt, type='o', col=2, pch=18, ylim=c(-15,15), xlab='', ylab='')
 par(new=TRUE)
 plot(svd_res(res_h)$kt, type='o', col=3, pch=20, ylim=c(-15,15), xlab='', ylab='')
 abline(h=0,lty=3)
-legend(15, -5, legend=c("Mujeres", "Hombres"), col=c("red", "green"), cex=0.9, lwd=3, bty = 'n')
+legend(15, -5, legend=c("Hombres","Mujeres"), col=c("green","red"),  bty = 'n', cex=0.9, 
+       pt.cex=2, inset=0.2, pch=c(20,18))
 
 # Ratio ajuste incluye residuos SVD
-nres_m <- log(qxt_m) - LCT_m$ax - fm(LCT_t$bx,2)%*%fm(LCT_t$kt,1) - fm(svd_res(res_m)$bx,2)%*%fm(svd_res(res_m)$kt,1)
-nres_h <- log(qxt_h) - LCT_h$ax - fm(LCT_t$bx,2)%*%fm(LCT_t$kt,1) - fm(svd_res(res_h)$bx,2)%*%fm(svd_res(res_h)$kt,1)
+nres_m <- log(mxt_m) - LCT_m$ax - fm(LCT_t$bx,2)%*%fm(LCT_t$kt,1) - fm(svd_res(res_m)$bx,2)%*%fm(svd_res(res_m)$kt,1)
+nres_h <- log(mxt_h) - LCT_h$ax - fm(LCT_t$bx,2)%*%fm(LCT_t$kt,1) - fm(svd_res(res_h)$bx,2)%*%fm(svd_res(res_h)$kt,1)
 Rca_m <- 1-(sum(nres_m^2))/(sum(pres_m^2))
 Rca_m
 Rca_h <- 1-(sum(nres_h^2))/(sum(pres_h^2))
@@ -415,13 +425,13 @@ tpm <- ts(svd_res(res_m)$kt, start=c(1965, 1), end=c(2004, 1), frequency=1)
 tph <- ts(svd_res(res_h)$kt, start=c(1965, 1), end=c(2004, 1), frequency=1)
 
 pry_m <- forecast(auto.arima(tpm), h=50, level=c(90,95))
-plot(pry_m, col=1, fcol=1, shadecols="lightgreen")
+plot(pry_m, col=1, fcol=1, shadecols="lightpink")
 pry_h<- forecast(auto.arima(tph), h=50, level=c(90,95))
-plot(pry_h, col=1, fcol=1, shadecols="lightpink")
+plot(pry_h, col=1, fcol=1, shadecols="lightgreen")
 
 
 li_qxt <- function(LCa, LCTb, res, pryt, pry){
-      estm <- exp(LCa$ax - fm(LCTb$bx,2)%*%fm(as.numeric(pryt$mean),1) - fm(svd_res(res_m)$bx,2)%*%fm(as.numeric(pry$mean),1))
+      estm <- exp(LCa$ax + fm(LCTb$bx,2)%*%fm(as.numeric(pryt$mean),1) + fm(svd_res(res_m)$bx,2)%*%fm(as.numeric(pry$mean),1))
       q <- 2*estm/(2+estm)
       colnames(q) <- seq(2005, 2054)
       return(q)
@@ -429,7 +439,7 @@ li_qxt <- function(LCa, LCTb, res, pryt, pry){
 
 
 # Gráfico tridimensional - Log probabilidades de muerte
-z <- log(li_qxt(LCT_h, LCT_t, res_h, pry_t, pry_h))
+z <- log(q1(li_qxt(LCT_m, LCT_t, res_m, pry_t, pry_m)))
 edad <- seq(0, 105, by=1)
 tiempo <- seq(2005, 2054, by=1)
 color <- colorRampPalette(c("green", "yellow", "orange", "red"))(50)
@@ -459,31 +469,42 @@ epam <- function(datar, datae){
       return(error)
 }
 
+# Almacenamiento de resultados
+LCPm <- q1(for_qxt(est_m))
+LCPh <- q1(for_qxt(est_h))
+CBDm <- q1(lg_qxt(pro_m))
+CBDh <- q1(lg_qxt(pro_h))
+LLm <- q1(li_qxt(LCT_m, LCT_t, res_m, pry_t, pry_m))
+LLh <- q1(li_qxt(LCT_h, LCT_t, res_h, pry_t, pry_h))
+save(list = c("LCPm", "LCPh", "LLm", "LLh", "CBDm", "CBDh"), file = "Estimaciones.RData", envir = .GlobalEnv)
 
 # LCP
-ecm(ctasas(Dxt_m,Pxt_m,56,65), for_qxt(est_m)[,c(1:10)])
-ecm(ctasas(Dxt_h,Pxt_h,56,65), for_qxt(est_h)[,c(1:10)])
+em1 <- ecm(ctasas(Dxt_m,Pxt_m,56,65)[15:106,], for_qxt(est_m)[15:106,c(1:10)])
+eh1 <- ecm(ctasas(Dxt_h,Pxt_h,56,65)[15:106,], for_qxt(est_h)[15:106,c(1:10)])
 
-epam(ctasas(Dxt_m,Pxt_m,56,65), for_qxt(est_m)[,c(1:10)])
-epam(ctasas(Dxt_h,Pxt_h,56,65), for_qxt(est_h)[,c(1:10)])
+em4 <- epam(ctasas(Dxt_m,Pxt_m,56,65)[15:106,], for_qxt(est_m)[15:106,c(1:10)])
+eh4 <- epam(ctasas(Dxt_h,Pxt_h,56,65)[15:106,], for_qxt(est_h)[15:106,c(1:10)])
 
 # LL
-ecm(ctasas(Dxt_m,Pxt_m,56,65), lg_qxt(pro_m)[,c(1:10)])
-ecm(ctasas(Dxt_h,Pxt_h,56,65), lg_qxt(pro_h)[,c(1:10)])
+em2 <- ecm(ctasas(Dxt_m,Pxt_m,56,65)[15:106,], li_qxt(LCT_m, LCT_t, res_m, pry_t, pry_m)[15:106,c(1:10)])
+eh2 <- ecm(ctasas(Dxt_h,Pxt_h,56,65)[15:106,], li_qxt(LCT_h, LCT_t, res_h, pry_t, pry_h)[15:106,c(1:10)])
 
-epam(ctasas(Dxt_m,Pxt_m,56,65), lg_qxt(pro_m)[,c(1:10)])
-epam(ctasas(Dxt_h,Pxt_h,56,65), lg_qxt(pro_h)[,c(1:10)])
+em5 <- epam(ctasas(Dxt_m,Pxt_m,56,65)[15:106,], li_qxt(LCT_m, LCT_t, res_m, pry_t, pry_m)[15:106,c(1:10)])
+eh5 <- epam(ctasas(Dxt_h,Pxt_h,56,65)[15:106,], li_qxt(LCT_h, LCT_t, res_h, pry_t, pry_h)[15:106,c(1:10)])
 
 # CBD
-ecm(ctasas(Dxt_m,Pxt_m,56,65), li_qxt(LCT_m, LCT_t, res_m, pry_t, pry_m)[,c(1:10)])
-ecm(ctasas(Dxt_h,Pxt_h,56,65), li_qxt(LCT_h, LCT_t, res_h, pry_t, pry_h)[,c(1:10)])
+em3 <- ecm(ctasas(Dxt_m,Pxt_m,56,65)[15:106,], lg_qxt(pro_m)[15:106,c(1:10)])
+eh3 <- ecm(ctasas(Dxt_h,Pxt_h,56,65)[15:106,], lg_qxt(pro_h)[15:106,c(1:10)])
 
-epam(ctasas(Dxt_m,Pxt_m,56,65), li_qxt(LCT_m, LCT_t, res_m, pry_t, pry_m)[,c(1:10)])
-epam(ctasas(Dxt_h,Pxt_h,56,65), li_qxt(LCT_h, LCT_t, res_h, pry_t, pry_h)[,c(1:10)])
+em6 <- epam(ctasas(Dxt_m,Pxt_m,56,65)[15:106,], lg_qxt(pro_m)[15:106,c(1:10)])
+eh6 <- epam(ctasas(Dxt_h,Pxt_h,56,65)[15:106,], lg_qxt(pro_h)[15:106,c(1:10)])
+
+xtable(data.frame(LCP=eh1, LL=eh2, CBD=eh3, LCP1=eh4, LL1=eh5, CBD1=eh6), digits=6)
+
+xtable(data.frame(LCP=em1, LL=em2, CBD=em3, LCP1=em4, LL1=em5, CBD1=em6), digits=6)
 
 
-# Graficos comparativos
+# Tiempo ejecución & Limpieza Workspace
+(Sys.time() - start)
+rm(list = ls())
 
-
-
-time <- start - Sys.time()
