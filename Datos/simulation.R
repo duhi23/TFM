1fdcb1f88f7ee02e62328c370a778c72b9c154a7
duhi23 <diego.21.phs@hotmail.com>
2018-06-06 14:59:15 +0200

    Act. Plan Pensiones

--- Datos/simulation.R
+++ Datos/simulation.R
@@ -42,6 +42,18 @@ TPh <- q1(qxt_perm(perm, proy=97, genero="h"))[1:106,-seq(1,4)]
 TPm <- q1(qxt_perm(perm, proy=97, genero="m"))[1:106,-seq(1,4)]
 rm(list=c("perm", "qxt_perm"))
 
+pasem <- read_excel("Datos/Tablas_PERMF2000.xlsx", sheet = 3) %>% dplyr::select(-Edad)
+rep_col <- function(vec, col){
+      res <- matrix(0, nrow=length(vec), ncol=col)
+      for(j in 1:col){
+            res[,j] <- vec
+      }
+      return(res)
+}
+
+PSh <- q1(rep_col(pasem[[1]], col=93))
+PSm <- q1(rep_col(pasem[[2]], col=93))
+
 # Carga de resultados modelos LCP, LL, CBD
 load("Estimaciones.RData")
 
@@ -70,7 +82,7 @@ descuento <- function(tasa, num, tipo){
 
 seg_dif <- function(edad, dur, capi, tipo, cre, tabla){
       tabla <- tabla[,-seq(1,13)] # filtramos periodo (2018-2097)
-      prb <- cumprod(1 - diag(tabla[(edad+1):(edad+dur+1),]))
+      prb <- cumprod(1 - diag(tabla[(edad+1):(edad+dur),]))
       psv <- prb[length(prb)]
       pxt <- c(1,prb[-length(prb)]) 
       res <- matrix(0, nrow=length(pxt), ncol=5)
@@ -82,95 +94,84 @@ seg_dif <- function(edad, dur, capi, tipo, cre, tabla){
       prima <- vap / sum(res[,3])
       res[,4] <- round(prima * res[,2],2)
       res[,5] <- round(prima * res[,3],2)
-      return(list(res,vap,prima))
+      return(list(res,vap, sum(res[,3]),prima))
 }
 
-seg_dif(edad=30, dur=20, capi=100000, tipo=0.02, cre=0.01, TPh)
 
+tab1 <- seg_dif(edad=45, dur=20, capi=100000, tipo=0.02, cre=0.01, PSh)
+tab2 <- seg_dif(edad=45, dur=20, capi=100000, tipo=0.02, cre=0.01, LLh)
 
-### Seguro Mixto
-# Cálculo de primas
-smixto <- function(mqxt, edad, dur, inte, capi=1){
-      mqxt <- mqxt[,-seq(1,13)] # se excluye la mortalidad anterior a 2018
-      dif <- 105 - edad
-      if(dif < dur){
-            cat("Duración no adecuada, se realiza el cálculo con duración de ", dif, " años\n")
-            qxt <- diag(mqxt[(edad+1):106,])
-      } else {
-            qxt <- diag(mqxt[(edad+1):(edad+dur),])
-      }
-      n <- length(qxt)
-      pxta <- cumprod(1-qxt)
-      st <- cumprod(rep(1/(1+inte), times=n))
-      smuerte <- sum(st * c(1,pxta[-n]) * qxt)
-      ssuper <- st[n] * pxta[n]
-      return(round(capi*(smuerte+ssuper),2))
+dif_tabla <- function(tabla1, tabla2){
+      res <- matrix(0, nrow=nrow(tabla1[[1]]), ncol=6)
+      row.names(res) <- row.names(tabla1[[1]])
+      res[,1] <- tabla1[[1]][,4] # cobrado
+      res[,2] <- tabla1[[1]][,5] # cobrado descontado
+      res[,3] <- tabla2[[1]][,4] # simulado
+      res[,4] <- tabla2[[1]][,5] # simulado descontado
+      res[,5] <- res[,1] - res[,3]
+      res[,6] <- res[,2] - res[,4]
+      rlong <- sum(res[,6])
+      return(list(res,rlong))
 }
 
-#smixto(LLh, edad=30, dur=5, inte=0.02, capi=100000)
+dif_tabla(tab1, tab2)
 
-iter_mixto <- function(tabla, iedad, idur, tipo, capital){
-      res <- matrix(0, ncol=length(idur), nrow=length(iedad))
-      colnames(res) <- idur
-      rownames(res) <- iedad
-      for(i in seq_along(iedad)){
-            for(j in seq_along(idur)){
-                  res[i,j] <- smixto(tabla, iedad[i], idur[j], tipo, capital)
+# Simulación riesgo de longevidad
+sim_long <- function(vec1, vec2, tab1, tab2){
+      res <- matrix(0, nrow=length(vec1), ncol=length(vec2))
+      for(i in 1:length(vec1)){
+            for(j in 1:length(vec2)){
+                  if(vec1[i] + vec2[j] <= 65){
+                        tab01 <- seg_dif(edad=vec1[i], dur=vec2[j], capi=100000, tipo=0.06, cre=0.01, tab1)
+                        tab02 <- seg_dif(edad=vec1[i], dur=vec2[j], capi=100000, tipo=0.06, cre=0.01, tab2)
+                        res[i,j] <- dif_tabla(tab01, tab02)[[2]]
+                  } else {
+                        res[i,j] <- 0
+                  }
             }
       }
       return(res)
 }
 
-iter_mixto(LLm, seq(30,60, by=5), seq(5,30, by=5), tipo=0.02, capital=100000)
+sim_long(seq(30,60,by=5), seq(5,35, by=5), PSm, LLm)
+sum(sim_long(seq(30,60,by=5), seq(5,35, by=5), PSm, LLm))
+
+
 
 # Plan de Pensiones (renta anual vitalicia)
-aportacion <- function(mqxt, edad, inte, alfa=1, beta=1, prest=1 ,salida=65){
-      mqxt <- mqxt[,-seq(1,13)] # se excluye la mortalidad anterior a 2018
-      if(edad >= salida){
-            cat("Edad de ingreso debe ser inferior a", salida, " años\n")
-            break
-      } else {
-            qxt <- diag(mqxt[(edad+1):106,])
-            pxta <- cumprod(1-qxt) # probabilidades de supervivencia acumuladas
-            st <- cumprod(rep(1/(1+inte), times=length(pxta)))
-      }
-      dur <- salida - edad
-      # flujos aportaciones
-      p1 <- c(1,pxta[1:(dur-1)])
-      st1 <- c(1,st[1:(dur-1)])
-      axr <- sum(p1 * st1)
-      # factor de actualizacion
-      p2 <- pxta[dur]
-      st2 <- st[dur]
-      rEx <- p2 * st2
-      # flujos prestaciones
-      p3 <- pxta[(dur+1):length(pxta)]
-      st3 <- st[(dur+1):length(st)]
-      ax <- sum(p3 * st3)
-      # calculo del aporte
-      val <- (rEx * ax)/axr
-      return(round(prest*val,2))
+aportacion <- function(mqxt1, mqxt2, edad, inte, alfa=0.01, beta=0.01,salida=65, prest=1){
+      mqxt1 <- mqxt1[,-seq(1,13)] # se excluye la mortalidad anterior a 2018
+      mqxt2 <- mqxt2[,-seq(1,13)] # se excluye la mortalidad anterior a 2018
+      pxt1 <- cumprod(1 - diag(mqxt1[(edad+1):106,]))
+      pxt2 <- cumprod(1 - diag(mqxt2[(edad+1):106,]))
+      des <- descuento(inte, length(pxt1)+1, "pre")
+      # Tabla aportaciones
+      pini <- c(1,pxt1[1:(salida-edad-1)])
+      tabini <- matrix(0, nrow=length(pini), ncol=5)
+      colnames(tabini) <- c("tpx", "nom", "va", "vaa", "vaax")
+      tabini[,1] <- pini
+      tabini[,2] <- prest * nominal(alfa, length(pini))
+      tabini[,3] <- tabini[,2] * des[1:length(pini)]
+      tabini[,4] <- tabini[,1] * tabini[,3]
+      tabini[,5] <- c(1,pxt2[1:(salida-edad-1)]) * tabini[,3]
+      # Tabla prestaciones
+      pfin <- pxt1[-c(1:(salida-edad-1))]
+      tabfin <- matrix(0, nrow=length(pfin), ncol=5)
+      tabfin[,1] <- pfin
+      tabfin[,2] <- nominal(beta, length(pfin))
+      tabfin[,3] <- tabfin[,2] * des[-c(1:length(pini))]
+      tabfin[,4] <- tabfin[,1] * tabfin[,3]
+      tabfin[,5] <- pxt2[-c(1:(salida-edad-1))] * tabfin[,3]
+      # Prima
+      prima <- sum(tabini[,4])/sum(tabfin[,4])
+      # Tabla final con todos los flujos +/-
+      res <- rbind(tabini[,-c(1)], -prima*tabfin[,-c(1)])
+      rlong <- round(apply(res[,c(3,4)],2,sum),2)
+      return(list(tabini,tabfin,prima,res,rlong))
 }
 
-#aportacion(TPm, edad=60, inte=0.04)
-  
-iter_apor <- function(tabla1, tabla2, iedad, inte, alfa, beta, capital=1){
-      res <- matrix(0, nrow = length(iedad), ncol=2)
-      for(i in seq_along(iedad)){
-            res[i,1] <- aportacion(tabla1, edad=iedad[i], inte, alfa, beta, capital)
-            res[i,2] <- aportacion(tabla2, edad=iedad[i], inte, alfa, beta, capital)
-      }
-      rownames(res) <- iedad
-      return(res)
-}
+aportacion(PSh, LLh, edad=40, inte=0.01, prest=1000)
 
-iter_apor(TPh, LLh, seq(30,60,by=5), inte=0.03, alfa=1, beta=1, capital=10000)
-
-
-# Cálculo de la reserva matemática (método retrospectivo)
-reserva <- function(mqxt, edad, inte, alfa=1, beta=1, salida=65){
-      
-}
 
 (Sys.time() - start)
 
